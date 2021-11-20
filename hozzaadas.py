855956f040ddc43dcf40a13652f0bba9e60dd192
tamaasd <tamas9182@gmail.com>
2021-11-20 22:46:56 +0100

    hozzaadas.py javítva

--- hozzaadas.py
+++ hozzaadas.py
@@ -1,16 +1,16 @@
 import os.path
 def addPainting():
     while True:
-        melyik_fajl=input("\n\tMelyik fájlban szeretnél menteni, ird be a nevét: ")
+        melyik_fajl=input("\n\tMelyik fájlban szeretne menteni?irja be a nevét, és kiterjesztését: ")
         if os.path.isfile(melyik_fajl):
             break
-        print("\tNem létezik a fájl, próbáld újra!")
+        print("\tNem létezik a fájl, próbálja újra!")
     f=open(melyik_fajl, 'a')
-    festmenynev=input("\tÍrd be a festmény nevét: ")
-    festmenyar=input("\tÍrd be a festmény árát[Ft]: ")
+    festmenynev=input("\tÍrja be a festmény nevét: ")
+    festmenyar=input("\tÍrja be a festmény árát[Ft]: ")
     f.write(f'{festmenynev};{festmenyar}\n')
     f.close()
-    print("\tMentve")
-    input("\tMenübe való vissza téréshez nyomj Entert!")
+    print("\tMentve\n")
+    input("\tMenübe való vissza téréshez nyomjon Entert!\n")
 
 addPainting()
