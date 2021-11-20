4e7724e6d3f91c23672cc95742627dfba0f72ee6
tamaasd <tamas9182@gmail.com>
2021-11-20 22:32:24 +0100

    ujfajl.py javitva

--- ujfajl.py
+++ ujfajl.py
@@ -2,18 +2,23 @@ import os.path
 
 def creat_newf():
     while True:
-        uj_fajl_nev=input("\n\tÍrd be az új fájl nevét: ")
+        uj_fajl_nev=input("\n\tÍrja be az új fájl nevét: ")
         if os.path.isfile(uj_fajl_nev):
-            print("Már létezik!")
-            igen_nem=input("\tAkarsz új fájl nevet megadni? [i/n]: ")
+            
+            
+            print("\tA fáj már létezik!\n")
+            
+            igen_nem=input("\tAkar új fájl nevet megadni?[i/n]: ")
             if igen_nem=="n":
                 break
             elif igen_nem!="i":
                 print("\tNajó... próbáld újra...")
+                
+                
         else:
             open(uj_fajl_nev, 'a').close()
             print("\tFájl létrehozva")
             break
-    input("\tMenübe való vissza téréshez nyomj Entert!")
+    input("\n\tMenübe való vissza téréshez nyomj Entert!\n")
 
 creat_newf()   
